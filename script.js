d2ee834aeeb78c235cfe12c692b9b7b841743b74
jpk1414 <jpk1414@gmail.com>
2026-08-07 15:51:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.querySelectorAll('.btn').forEach(b=>{
-b.addEventListener('mouseenter',()=>b.style.background='rgba(233,89,196,.18)');
-b.addEventListener('mouseleave',()=>b.style.background='rgba(0,0,0,.45)');
-});
+// Reserved for future enhancements
